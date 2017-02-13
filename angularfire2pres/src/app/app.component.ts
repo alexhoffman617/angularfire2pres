f41c1e817a1e7386059095584d97fcaa8dcfcf79
Alex Hoffman <alexhoffman617@gmail.com>
2017-02-13 12:58:54 -0500

    Deployed to firebase

--- angularfire2pres/src/app/app.component.ts
+++ angularfire2pres/src/app/app.component.ts
@@ -1,10 +1,76 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewChecked, ElementRef, ViewChild, OnInit } from '@angular/core';
+import { AngularFire, FirebaseListObservable } from 'angularfire2';
+import * as firebase from 'firebase';
+import { MdButton, MdInput, MdList, MdListItem, MdIcon } from '@angular/material';
+import { AngularFireModule, AuthProviders, AuthMethods} from 'angularfire2';
+
+
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app works!';
+export class AppComponent implements OnInit, AfterViewChecked {
+    af;
+    user;
+    inputText: '';
+    ngOnInit() {
+      this.scrollMessagesToBottom();
+    }
+
+    ngAfterViewChecked() {
+      this.scrollMessagesToBottom();
+    }
+
+    scrollMessagesToBottom() {
+      var messagesDiv = document.getElementById('messages');
+      messagesDiv.scrollTop = messagesDiv.scrollHeight;
+    }
+    messages: FirebaseListObservable<any[]>;
+    login() {
+      this.af.auth.login({
+
+      });
+    }
+
+    logout() {
+      this.af.auth.logout();
+    }
+
+    submitMessage(text){
+      var username = this.user ? this.user.auth.displayName: 'Anonymous';
+      var photo = this.user ? this.user.auth.photoURL: 'http://cdn.onlinewebfonts.com/svg/img_210318.svg';
+         this.messages.push({
+           time: firebase.database.ServerValue.TIMESTAMP,
+           name: username,
+           text: text,
+           photo: photo
+         });
+         this.inputText = '';
+    };
+    formatDate(date) {
+      var actualDate = new Date(date);
+      return actualDate.toTimeString().substr(0,5);
+    }
+    constructor(af: AngularFire) {
+        this.af = af;
+         this.messages = af.database.list('/messages', { 
+           query: {
+             orderByChild: 'time'
+           }
+         });
+
+      this.af.auth.subscribe(user => {
+        if(user) {
+          // user logged in
+          this.user = user;
+        }
+        else {
+          // user not logged in
+          this.user = null;
+        }
+    });
+  }
 }
--- angularfire2pres/src/app/app.module.ts
+++ angularfire2pres/src/app/app.module.ts
@@ -1,20 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
-
 import { AppComponent } from './app.component';
+import { AngularFireModule, AuthProviders, AuthMethods, AngularFire} from 'angularfire2';
+import { FormsModule } from '@angular/forms';
+import { MaterialModule } from '@angular/material';
+
+export const firebaseConfig = {
+    apiKey: "AIzaSyAxb2bqyFPng5Tee0MZrHZ5PbACPN2_zCg",
+    authDomain: "angularfire2demo.firebaseapp.com",
+    databaseURL: "https://angularfire2demo.firebaseio.com",
+    storageBucket: "angularfire2demo.appspot.com",
+    messagingSenderId: "1088505020716"
+};
+
+const myFirebaseAuthConfig = {
+  provider: AuthProviders.Google,
+  method: AuthMethods.Redirect,
+  remember: 'default', 
+};
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
     BrowserModule,
+    AngularFireModule.initializeApp(firebaseConfig, myFirebaseAuthConfig),
     FormsModule,
-    HttpModule
+    MaterialModule.forRoot()
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [ AppComponent ],
+  bootstrap: [ AppComponent ]
 })
-export class AppModule { }
+
+export class AppModule { 
+}
